bf95b0ea677aafed915140deaa87f43e184cdbcd
JohnPlayerSpecial <cs.prio30@gmail.com>
2017-10-07 13:03:28 +0200

    Update bottarelli.py

--- bottarelli.py
+++ bottarelli.py
@@ -18,74 +18,72 @@ from newspaper import Article
 from urllib.request import urlopen 
 
 
-API_KEY_MERCURY_PARSER = 'nGc0ya2J7z2aalFrGa8Gx3Q1o8grGFsn3cz58EJy'
+STRING_DB = os.environ['DATABASE_URL'].replace("postgres","pq")
+API_KEY_MERCURY_PARSER = os.environ['API_KEY_MERCURY_PARSER']  
+TOKEN_TELEGRAM = os.environ['TOKEN_TELEGRAM'] 
+MY_CHAT_ID = int( os.environ['MY_CHAT_ID'] )
+HOUR_I_WANNA_GET_MESSAGE = int( os.environ['HOUR_I_WANNA_GET_MESSAGE'] )
+MINUTES_I_WANNA_GET_MESSAGE = int( os.environ['MINUTES_I_WANNA_GET_MESSAGE'] )
+
 parser = MercuryParser(api_key = API_KEY_MERCURY_PARSER)
-
 feed = 'http://www.ilsussidiario.net/Feed/autore/230/Mauro-Bottarelli/'
-TOKEN_TELEGRAM = '423757798:AAF2T68VYPWcw9-t06qV6vcXvvNkMRMIROg'
-
 telegraph = Telegraph()
 telegraph.createAccount('TELEGRAPH_ACCOUNT')
 
+def init_DB():
+	global STRING_DB
+	db = postgresql.open(STRING_DB)
+	ps = db.prepare("CREATE TABLE IF NOT EXISTS url (id serial PRIMARY KEY, url varchar(300) unique);")
+	ps()          
+	db.close()
+
 def getArticles( bot, job ):
+	global STRING_DB
+	db = postgresql.open(STRING_DB)
+	ps = db.prepare("SELECT * FROM url;")
+	allUrl = [ item[1] for item in ps() ]
 	entries = feedparser.parse( feed ).entries
-	for post in tqdm(entries):
-		try:
-			print(post.link)
-			article = parser.parse_article( post.link )
-			title = article.json()['title'].replace("SPY FINANZA/ ","")
-			lead_image_url = article.json()['lead_image_url']
-			excerpt = article.json()['excerpt']
-			word_count = article.json()['word_count']
-			content = article.json()['content']
-			article = Article( post.link )
-			article.download()
-			article.parse()
-			text = article.text
-			#print(text)
-			articleImage = article.top_image
-			articleTitle = article.title.replace("SPY FINANZA/ ","")
-			articleUrl = article.url
-			html = urlopen( post.link )
-			html_content = BeautifulSoup( html , 'html.parser' ).findAll("p", {"style":"text-align: left;"})
-			contentWithImages = ''
-			for item in html_content:
-				contentWithImages += str(item).replace("<span>","").replace('<span class="s1">',"").replace('</span>',"")
-			
-			contentWithImages = '<a href="{}"><img src="{}"/></a>'.format(articleImage, articleImage) + contentWithImages
-			#print(contentWithImages)
-			
-			page = telegraph.createPage( title = title,  html_content = contentWithImages , author_name = "Mauro Bottarelli" )
-			url2send = 'http://telegra.ph/' + page['path']
-			dictDays = { "Mon" : "Lunedì",
-			             "Tue" : "Martedì",
-			             "Wed" : "Mercoledì",
-			             "Thu" : "Giovedì",
-			             "Fri" : "Venerdì",
-			             "Sat" : "Sabato",
-			             "Sun" : "Domenica",		     
-			           }
-			dictMonths = {  "Jan" : "Gennaio",
-			                "Feb" : "Febbraio",
-			                "Mar" : "Marzo",
-			                "Apr" : "Aprile",
-			                "May" : "Maggio",
-			                "Jun" : "Giugno",
-			                "Jul" : "Luglio",	
-			                "Aug" : "Agosto",	
-			                "Sep" : "Settembre",	
-			                "Oct" : "Ottobre",	
-			                "Nov" : "Novembre",	
-			                "Dec" : "Dicembre",		     
-			           }
-			published = post.published
-			datePub = published.replace("+0200","")
-			
-			text = '<a href="{}">{}</a><b>{}</b>\n<i>{}</i>\n\n<b>{} parole, {} minuti ca. di lettura\n</b>Pubblicato {} da <a href="{}">ilsussidiario.net</a>\n{}'.format( url2send, u"\u2063", articleTitle, excerpt.replace("MAURO BOTTARELLI","Mauro Bottarelli"), word_count, int(word_count/235), datePub, post.link, u"\u2063" )
-			bot.sendMessage(chat_id=31923577, text = text, parse_mode = "Html")
-			time.sleep(0.1)
-		except Exception as e:
-			print(e)
+	for post in entries:
+		if post.link not in allUrl:
+			try:
+				ps = db.prepare("INSERT INTO url (url) VALUES ('{}') ON CONFLICT (url) DO NOTHING;".format(post.link) )
+				ps()
+				#print(post.link)
+				article = parser.parse_article( post.link )
+				title = article.json()['title'].replace("SPY FINANZA/ ","")
+				lead_image_url = article.json()['lead_image_url']
+				excerpt = article.json()['excerpt']
+				word_count = article.json()['word_count']
+				content = article.json()['content']
+				article = Article( post.link )
+				article.download()
+				article.parse()
+				text = article.text
+				#print(text)
+				articleImage = article.top_image
+				articleTitle = article.title.replace("SPY FINANZA/ ","")
+				articleUrl = article.url
+				html = urlopen( post.link )
+				html_content = BeautifulSoup( html , 'html.parser' ).findAll("p", {"style":"text-align: left;"})
+				contentWithImages = ''
+				for item in html_content:
+					contentWithImages += str(item).replace("<span>","").replace('<span class="s1">',"").replace('</span>',"")
+
+				contentWithImages = '<a href="{}"><img src="{}"/></a>'.format(articleImage, articleImage) + contentWithImages
+				#print(contentWithImages)
+
+				page = telegraph.createPage( title = title,  html_content = contentWithImages , author_name = "Mauro Bottarelli" )
+				url2send = 'http://telegra.ph/' + page['path']
+				published = post.published
+				datePub = published.replace("+0200","")
+
+				text = '<a href="{}">{}</a><b>{}</b>\n<i>{}</i>\n\n<b>{} parole, {} minuti ca. di lettura\n</b>Pubblicato {} da <a href="{}">ilsussidiario.net</a>\n{}'.format( url2send, u"\u2063", articleTitle, excerpt.replace("MAURO BOTTARELLI","Mauro Bottarelli"), word_count, int(word_count/235), datePub, post.link, u"\u2063" )
+				bot.sendMessage(chat_id=MY_CHAT_ID, text = text, parse_mode = "Html")
+				time.sleep(0.1)
+			except Exception as e:
+				print(e)
+		else:
+			pass
 			
 def start( bot, update ):
 	bot.sendMessage(chat_id = update.message.chat_id, text = "Successfully subscribed.")
@@ -95,15 +93,12 @@ dp = updater.dispatcher
 updater.dispatcher.add_handler(CommandHandler('start', start))
 
 j = updater.job_queue
-
-
 utc_offset_heroku = time.localtime().tm_gmtoff / 3600
 
-future = datetime.datetime.now() + timedelta(seconds=1) 
-
-time2 = datetime.time(future.hour, future.minute, future.second)
-print(time2)
+hour = HOUR_I_WANNA_GET_MESSAGE + ( int(utc_offset_heroku) - 2 ) # 2 is my offset
+time2 = datetime.time(hour , MINUTES_I_WANNA_GET_MESSAGE)
+time3 = datetime.time(hour + 12 , MINUTES_I_WANNA_GET_MESSAGE)
 j.run_daily(getArticles, time2 )
-
+j.run_daily(getArticles, time3 )
 updater.start_polling()
 updater.idle()
